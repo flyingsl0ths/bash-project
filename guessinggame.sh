ccc4128f7f1248be94ae06c4f98eff987a960aa1
flyingsl0ths <randomemail32110@gmail.com>
2019-12-06 19:26:42 -0600

    Refactored guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -3,28 +3,25 @@
 
 function guessingGame()
 {
-  local numFiles=0
-  local get_num=0
-
-  for f in $(ls -A)
-  do
-    let numFiles++
-  done
+  local numFiles=$(ls -A | wc -l)
 
   echo "GUESSING GAME"
 
-  unset get_num
-  while [[ ! $get_num = $numFiles ]]
+  while [[ $get_num -ne $numFiles ]]
   do
     echo "Guess how many files are in this directory"
     read get_num
-    if ! [[ ${get_num} -ge 0 && ${get_num} -le $numFiles ]]
+    if [[ $get_num -ge $numFiles ]]
     then
-      echo "Nope try again"
-      unset get_num
+      echo "Too High"
+    elif [[ $get_num -le $numFiles ]]
+    then
+      echo "Too low"
     fi
   done
+  
   echo "Correct there are $get_num files in this directory!!"
 }
 
 guessingGame
+
